fccffc4d0cc5c190e6e9b2251b7d15dff2fdf61f
Hamza <info@hamzahmetoglu.com>
2021-09-12 13:27:19 +0300

    second commit

--- src/validations/todo.js
+++ src/validations/todo.js
@@ -1,11 +1,19 @@
 const Joi = require("joi");
 
 module.exports = {
-  createTodo: Joi.object().keys({
-    task: Joi.string().required().min(4),
-  }),
-  updateTodo: Joi.object().keys({
-    task: Joi.string().min(4).optional(),
-    status: Joi.string().optional().valid("active", "completed"),
-  }),
+  createTodo: Joi.object()
+    .keys({
+      task: Joi.string().required().min(4),
+    })
+    .options({
+      abortEarly: false,
+    }),
+  updateTodo: Joi.object()
+    .keys({
+      task: Joi.string().min(4).optional(),
+      status: Joi.string().optional().valid("active", "completed"),
+    })
+    .options({
+      abortEarly: false,
+    }),
 };
--- src/validations/user.js
+++ src/validations/user.js
@@ -1,12 +1,20 @@
 const Joi = require("joi");
 
 module.exports = {
-  register: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().required().min(4),
-  }),
-  login: Joi.object().keys({
-    email: Joi.string().email().required(),
-    password: Joi.string().required().min(4),
-  }),
+  register: Joi.object()
+    .keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().required().min(4),
+    })
+    .options({
+      abortEarly: false,
+    }),
+  login: Joi.object()
+    .keys({
+      email: Joi.string().email().required(),
+      password: Joi.string().required().min(4),
+    })
+    .options({
+      abortEarly: false,
+    }),
 };
