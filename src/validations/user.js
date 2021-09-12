f2b55536d6f7ca19539aaff36358a981421325c2
Hamza <info@hamzahmetoglu.com>
2021-09-12 13:24:28 +0300

    second commit

--- src/validations/user.js
+++ src/validations/user.js
@@ -2,11 +2,11 @@ const Joi = require("joi");
 
 module.exports = {
   register: Joi.object().keys({
-    email: Joi.string().email(),
+    email: Joi.string().email().required(),
     password: Joi.string().required().min(4),
   }),
   login: Joi.object().keys({
-    email: Joi.string().email(),
+    email: Joi.string().email().required(),
     password: Joi.string().required().min(4),
   }),
 };
